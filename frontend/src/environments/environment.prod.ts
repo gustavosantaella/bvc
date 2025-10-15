f080f53f3ef3a0c50a16ea1d32cfe284b837af45
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2025-10-15 16:38:08 -0400

    feat: deploy 9

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // Este archivo se usa para el entorno de PRODUCCIÓN
 export const environment = {
   production: true,
-  apiUrl: 'https://bvc-six.vercel.app/',
+  apiUrl: 'https://bvc-six.vercel.app',
   appName: 'BVC Frontend',
   version: '1.0.0',
 };
