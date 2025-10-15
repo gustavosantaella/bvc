44eeb98c6835d7105a2b2286408fd6e762fbda73
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2025-10-14 21:41:48 -0400

    fix: run functions

--- backend/main.py
+++ backend/main.py
@@ -3,29 +3,33 @@ from dotenv import load_dotenv
 from src.database.mongo import start_db
 import asyncio
 import src.ws.bvc as ws_bvc
+
 load_dotenv() 
 
 async def main():
-    # start_db()
-    task  =asyncio.create_task(ws_bvc.connect_to_ws_bvc())
+    start_db()
+    websocket_task = asyncio.create_task(ws_bvc.connect_to_ws_bvc())
+    
+    config = uvicorn.Config(
+        'src.app_module:http_server',
+        host="0.0.0.0",
+        port=8000,
+        reload=False
+    )
+    server = uvicorn.Server(config)
+    
+    
     try:
-        asyncio.run(await asyncio.Future())  # Se ejecuta indefinidamente
-    except KeyboardInterrupt:
-        print("Apagando...")
-        task.cancel()
-    print("Running")
-    # uvicorn.run(
-    #     'src.app_module:http_server',
-    #     host="0.0.0.0",
-    #     port=8000,
-    #     reload=True
-    # )
+        await asyncio.gather(
+            server.serve(),
+            websocket_task,
+            return_exceptions=False  
+        )
+    except Exception as e:
+        print(f"Error: {e}")
     
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("The program was stoped by the user.")
-
-    
-    
+        print("The program was stopped by the user.")
