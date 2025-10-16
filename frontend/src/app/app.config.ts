82b194ccc8fbc12b5cbea05a61bc620a3fb5fa4b
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2025-10-16 10:08:42 -0400

    feat: analytics

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,20 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    {
+      provide: 'Analytics',
+      useValue: inject(),
+    },
+    {
+      provide: 'SpeedInsights',
+      useValue: injectSpeedInsights(),
+    },
   ],
 };
